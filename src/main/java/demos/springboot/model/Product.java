d056bda604bc2d7bae7a13e90ff94229b6b4be2e
Atul3322 <kumaratul3322@gmail.com>
2022-05-16 14:50:56 +0530

    1st commit of project

---
+++ src/main/java/demos/springboot/model/Product.java
@@ -0,0 +1,90 @@
+package demos.springboot.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "ProductJPA")
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(name = "ProductJPA_productId_seq")
+	@Column(name = "ProductId")
+	private int productId;
+	
+	@Column(name = "ProductName")
+	private String productName;
+	
+	@Column(name = "Brand")
+	private String brand;
+	
+	@Column(name = "Price")
+	private double price;
+	
+	@ManyToOne//(cascade = CascadeType.ALL)
+	@JoinColumn(name = "CategoryId")
+	@JsonIgnoreProperties("products")
+	private Category category;
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Product(int productId, String productName, String brand, double price) {
+		super();
+		this.productId = productId;
+		this.productName = productName;
+		this.brand = brand;
+		this.price = price;
+	}
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	@Override
+	public String toString() {
+		return "Product [productId=" + productId + ", productName=" + productName + ", brand=" + brand + ", price="
+				+ price + "]";
+	}
+	
+	
+
+}
